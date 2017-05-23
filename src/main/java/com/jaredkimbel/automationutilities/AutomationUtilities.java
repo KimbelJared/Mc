4441fca4ddd621a7232bfc08fef61bb08c9bc976
Jared Kimbel <1255376@ad.ccsd.net>
2017-05-23 10:44:52 -0700

    Init

--- src/main/java/com/jaredkimbel/automationutilities/AutomationUtilities.java
+++ src/main/java/com/jaredkimbel/automationutilities/AutomationUtilities.java
@@ -1,15 +1,16 @@
-package com.example.examplemod;
+package com.jaredkimbel.automationutilities;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = AutomationUtilities.MODID, version = AutomationUtilities.VERSION)
+public class AutomationUtilities
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "AutomationUtils";
     public static final String VERSION = "1.0";
+    public static final String NAME = "Automation Utilities";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
