4082fee69de9c0c9e594a3fd5f2c68a2dd469df6
Jared Kimbel <1255376@ad.ccsd.net>
2017-05-23 14:01:14 -0700

    0.01 Complete
    Named mod,
    and packages sorted

--- src/main/java/com/jaredkimbel/automationutilities/AutomationUtilities.java
+++ src/main/java/com/jaredkimbel/automationutilities/AutomationUtilities.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 public class AutomationUtilities
 {
     public static final String MODID = "AutomationUtils";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "0.01";
     public static final String NAME = "Automation Utilities";
     
     @EventHandler
